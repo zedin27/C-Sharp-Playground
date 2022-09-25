a504db4eb2b26143437a980fe22a2cd625f77c90
zedin27 <zeidtisnes@gmail.com>
2022-09-25 17:29:22 +0300

    Playing around the language. Need to restructure later. More README

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # C-Sharp-Playground
+
 Introductory C# work and projects done
+
+This shows the learning experience from C# before jumping into Unity.
+
+My goals after understanding C# is to go through Unity. For each of the list, it will contain some notes in regards of the following:
+
+- [ ] Mammoth (Course for C#)
+- [ ] Flappy Bird
+- [ ] Tetris
+- [ ] Rubik's Cube
+
+## Mammoth (Course for C#)
+
+https://rextester.com/ZOVAT66306
+
+If I want to test C#, check out "rextester.com" ? Nah.
+Entry points varies through system to system
+
+## TODO:
+
+- [ ] Restructure directories
+- [ ]
---
+++ everything/csharpness_playground/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// namespace csharpness
+// {
+// 	class Program
+// 	{
+// 		static void Main(string[], args)
+// 		{
+// 			Console.WriteLine("Hello World!");
+// 		}
+// 	}
+// }
+static int add(int x, int y)
+{
+	int result = x + y;
+	return result;
+}
+
+// int MAX_INT = 0b1111111111111111111111111111111;
+// int MIN_INT = 0b1111111111111111111111111111111 << 31;
+// int x = 60;
+// double y = 60.60;
+// char z = 'z';
+// string str = "my string";
+
+Console.WriteLine(add(2, 2));
+float floatA = 2.3f;
+double doubleA = 12;
+sbyte sbyteA = 14;
+
+int intA = (int)doubleA;
+// short shortA = intA;
+Console.WriteLine(intA);
+double doubleB = 2.123456789;
+float floatB = (float)doubleB;
+Console.WriteLine(floatB);
+
+//Look into ToInt, ToString, and so on
