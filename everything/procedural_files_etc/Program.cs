e6311d63f27c97c3754560e9888643e5e61cf304
zedin27 <zeidtisnes@gmail.com>
2022-10-02 17:24:23 +0300

    added new directory to work on vectors

---
+++ everything/procedural_files_etc/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+** Procedural programming is making calls to functions/routines/subroutines
+** TODO:	Vector math class
+** 			Transofmration class (scaling, translation, rotation)
+** A-B:		Create a GUI with the implementation
+** 			Add code and interactive with inputs (readkey?)
+**			Read it from a file?
+**
+** A-B = above and beyond
+*/
+
+
+using System.IO;
+
+namespace procedural_files_etc
+{
+	public class Program
+	{
+		static int add(int x, int y)
+		{
+			Console.Write("Result: ");
+			return x + y;
+		}
+		public static void Main(string[] args)
+		{
+			Console.WriteLine(add(5,5));
+		}
+	}
+}
