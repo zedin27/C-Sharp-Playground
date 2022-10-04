c16201690617b7e3f1c5beb6b1de0e8e057ed100
zedin27 <zeidtisnes@gmail.com>
2022-10-04 19:00:07 +0300

    added addition, substraction, multiplication, division, dot product, and cross product for 3d (and 4d) vectors

--- everything/vectors_transformations_projections_etc/Program.cs
+++ everything/vectors_transformations_projections_etc/Program.cs
@@ -1,15 +1,13 @@
 ﻿/*
-** Procedural programming is making calls to functions/routines/subroutines
-** Tuples return value in a class
 ** Enumerables (IEnumerators and IEnumerables): https://www.youtube.com/watch?v=UfT-st9dl8Q&ab_channel=AngelSix
 ** IEnumerator vs. IEnumerable difference
 ** TODO:	Vector math class
-** 			Transofmration class (scaling, translation, rotation)
+**			Transofmration class (scaling, translation, rotation)
 ** A-B:		Create a GUI with the implementation
-** 			Add code and interactive with inputs (readkey?)
+**			Add code and interactive with inputs (readkey?)
 **			Read it from a file?
 **
-** A-B = above and beyond
+** note -> A-B above and beyond™
 */
 
 
@@ -21,6 +19,7 @@ namespace procedural_files_etc
 		public double x;
 		public double y;
 		public double z;
+		public double w;
 		public VectorMaths()
 		{
 
@@ -30,11 +29,12 @@ namespace procedural_files_etc
 			this.x = value;
 			this.y = value;
 			this.z = value;
+			this.w = value;
 		}
 		public VectorMaths(double x, double y)
 		{
-			this.x = x; //or this.x
-			this.y = y; //or this.y
+			this.x = x;
+			this.y = y;
 		}
 		public VectorMaths(double x, double y, double z)
 		{
@@ -42,10 +42,24 @@ namespace procedural_files_etc
 			this.y = y;
 			this.z = z;
 		}
+		public VectorMaths(double x, double y, double z, double w)
+		{
+			this.x = x;
+			this.y = y;
+			this.z = z;
+			this.w = w;
+		}
+		public static double Magnitude(VectorMaths value1)
+		{
+			double result;
+			result = Math.Sqrt((value1.x * value1.x) + (value1.y * value1.y) + (value1.z * value1.z));
+			return result;
+		}
 		public static VectorMaths Add(VectorMaths value1, VectorMaths value2)
 		{
 			value1.x += value2.x;
 			value1.y += value2.y;
+			value1.z += value2.z;
 			return value1;
 		}
 		public static void Add(ref VectorMaths value1, ref VectorMaths value2, out VectorMaths result)
@@ -53,18 +67,65 @@ namespace procedural_files_etc
 			result = new VectorMaths();
 			result.x = value1.x + value2.x;
 			result.y = value1.y + value2.y;
+			result.z = value1.z + value2.z;
 		}
-		public double substraction()
+		public static VectorMaths Substraction(VectorMaths value1, VectorMaths value2)
 		{
-			return 0;
+			value1.x -= value2.x;
+			value1.y -= value2.y;
+			value1.z -= value2.z;
+			return value1;
 		}
-		public double dot()
+		public static void Substraction(ref VectorMaths value1, ref VectorMaths value2, out VectorMaths result)
 		{
-			return 0;
+			result = new VectorMaths();
+			result.x = value1.x - value2.x;
+			result.y = value1.y - value2.y;
+			result.z = value1.z - value2.z;
 		}
-		public double cross()
+		public static VectorMaths Multiply(VectorMaths value1, VectorMaths value2)
 		{
-			return 0;
+			value1.x *= value2.x;
+			value1.y *= value2.y;
+			value1.z *= value2.z;	
+			return value1;
+		}
+		public static void Multiply(ref VectorMaths value1, ref VectorMaths value2, out VectorMaths result)
+		{
+			result = new VectorMaths();
+			result.x = value1.x * value2.x;
+			result.y = value1.y * value2.y;
+			result.z = value1.z * value2.z;
+		}
+			public static VectorMaths Divide(VectorMaths value1, VectorMaths value2)
+		{
+			value1.x /= value2.x;
+			value1.y /= value2.y;
+			value1.z /= value2.z;	
+			return value1;
+		}
+		public static void Divide(ref VectorMaths value1, ref VectorMaths value2, out VectorMaths result)
+		{
+			result = new VectorMaths();
+			result.x = value1.x / value2.x;
+			result.y = value1.y / value2.y;
+			result.z = value1.z / value2.z;
+		}
+		public static double Dot(VectorMaths value1, VectorMaths value2)
+		{
+			return (value1.x * value2.x) + (value1.y * value2.y) + (value1.z * value2.z);
+		}
+		public static void Dot(ref VectorMaths value1, ref VectorMaths value2, out double result)
+		{
+			result = (value1.x * value2.x) + (value1.y * value2.y) + (value1.z * value2.z);
+		}
+		public static VectorMaths Cross(VectorMaths value1, VectorMaths value2)
+		{
+			VectorMaths value3 = new VectorMaths();
+			value3.x = (value1.y * value2.z) - (value1.z * value2.y);
+			value3.y = (value1.z * value2.x) - (value1.x * value2.z);
+			value3.z = (value1.x * value2.y) - (value1.y * value2.x);
+			return value3;
 		}
 		public void finish()
 		{
@@ -82,26 +143,28 @@ namespace procedural_files_etc
 	{
 
 	}
+	
 
 	public class Program
 	{
-		//TODO: Tuple creations soon(tm)
-		static double start(double x, double y)
-		{
-			// var nums = new Tuple<double, double>(x, y); Check this one out instead of Tuple.Create()
-			Console.Write("Give x and y value: ");
-			VectorMaths values = new VectorMaths(x, y);
-			var line = Console.ReadLine();
-			var data = line.Split(' ');
-			values.x = double.Parse(data[0]);
-			values.y = double.Parse(data[1]);
-			var nums = Tuple.Create(values.x, values.y);
-			Console.WriteLine("This is a tuple with their item values: {0} {1}\n", nums.Item1, nums.Item2);
-			Console.WriteLine("This is a data test: {0} {1}\n", data[0], data[1]);
-			Console.Write("Tuple values of x and y: {0}\n", nums);
-			return values.x; //return it to x and y with out?
-		}
-		//TODO: Maybe try to figure out how to take the vector values in the parameters?
+		/*TODO: Tuple creations soon™*/
+		// static double start(double x, double y)
+		// {
+		// 	// var nums = new Tuple<double, double>(x, y); Check this one out instead of Tuple.Create()
+		// 	Console.Write("Give x and y value: ");
+		// 	VectorMaths values = new VectorMaths(x, y);
+		// 	var line = Console.ReadLine();
+		// 	var data = line.Split(' ');
+		// 	values.x = double.Parse(data[0]);
+		// 	values.y = double.Parse(data[1]);
+		// 	var nums = Tuple.Create(values.x, values.y);
+		// 	Console.WriteLine("This is a tuple with their item values: {0} {1}\n", nums.Item1, nums.Item2);
+		// 	Console.WriteLine("This is a data test: {0} {1}\n", data[0], data[1]);
+		// 	Console.Write("Tuple values of x and y: {0}\n", nums);
+		// 	return values.x; //return it to x and y with out?
+		// }
+		
+		/*TODO: Maybe try to figure out how to take the vector values in the parameters?*/
 		// 	static double start(double v1_x, double v1_y, double v2_x, double v2_y)
 		// {
 		// 	// var nums = new Tuple<double, double>(x, y); Check this one out instead of Tuple.Create()
@@ -123,8 +186,33 @@ namespace procedural_files_etc
 		{
 			VectorMaths v1 = new VectorMaths(2, 6);
 			VectorMaths v2 = new VectorMaths(10.15, 0.02);
+			VectorMaths v3 = new VectorMaths();
+			VectorMaths v4 = new VectorMaths(2, 3, 4);
+			VectorMaths v5 = new VectorMaths(5, 6, 7);
+			VectorMaths v6 = new VectorMaths(6, 8);
+			VectorMaths v7 = new VectorMaths(-6, 5);
+			VectorMaths v8 = new VectorMaths(8, 12);
+			VectorMaths v11 = new VectorMaths(2, 6);
+			VectorMaths v22 = new VectorMaths(10.15, 0.02);
+			VectorMaths v12 = new VectorMaths(1, 2, 3);
+			VectorMaths v13 = new VectorMaths(4, 5, 6);
+			VectorMaths v14 = new VectorMaths(1, 2, 3);
+			VectorMaths v15 = new VectorMaths(4, 5, 6);
+
+			double mag_val = VectorMaths.Magnitude(v6);
 			VectorMaths res = VectorMaths.Add(v1, v2);
-			Console.WriteLine(res.x + " " + res.y);
+			VectorMaths res0 = VectorMaths.Substraction(v11, v22);
+			VectorMaths res2 = VectorMaths.Multiply(v14, v15);
+			double res3 = VectorMaths.Dot(v12, v13);
+			VectorMaths res1 = VectorMaths.Cross(v4, v5);
+
+
+			Console.WriteLine($"Magnitude of vector is: {mag_val}");
+			Console.WriteLine($"Add vectors: ({res.x}, {res.y})");
+			Console.WriteLine($"Substract vectors: ({res0.x}, {res0.y})");
+			Console.WriteLine($"Multiply vectors: ({res2.x}, {res2.y}, {res2.z})");
+			Console.WriteLine($"Dot Product: {res3}");
+			Console.WriteLine($"Cross Product: ({res1.x}, {res1.y}, {res1.z})");
 		}
 	}
 }
