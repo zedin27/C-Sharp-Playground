2d82480512ac5892f3de2dd7b671ab298c7fe68b
zedin27 <zeidtisnes@gmail.com>
2022-10-25 15:40:07 +0300

    update: fixed my issue with the falling object after collision. It should run smooth like soap

--- everything/3D Project/Flappy Bird/Assets/Scripts/PlayerControl.cs
+++ everything/3D Project/Flappy Bird/Assets/Scripts/PlayerControl.cs
@@ -15,11 +15,11 @@ public class PlayerControl : MonoBehaviour
     public AudioSource sound_hit;
     public AudioSource sound_die;
     private Vector3 currentEuler;
+    public Transform target; // "grass_floor"
     public bool gameOver = false;
-    public bool hit = false;
-    private float upperBound = 9;
+    public bool hit;
+    private float upperBound = 9f;
     public float my_gravity = -9.81f;
-    public float fallSpeed = 0;
     public float upforce;
     private bool sleeping;
     private float minAngle = -69.0f;
@@ -33,6 +33,7 @@ public class PlayerControl : MonoBehaviour
         playerRb.drag = 5f;
         playerRb.mass = 9001.42f;
         sleeping = true;
+        hit = false;
         upforce = 6.66f;
         Time.timeScale = 0;
     }
@@ -47,13 +48,22 @@ public class PlayerControl : MonoBehaviour
             if (transform.position.y > upperBound)
                 transform.position = new Vector3(transform.position.x, upperBound, transform.position.z);
         }
+        else
+        {
+            if (transform.position.y >= target.position.y)
+            {
+                velocity.y +=  my_gravity * Time.deltaTime;
+                transform.position += velocity * Time.deltaTime;
+            }
+            GameOver();
+        }
     }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Grass_floor") || collision.gameObject.CompareTag("Pipe"))
         {
             HitAndDie();
-            // GameOver();
+            GameOver();
         }
     }
     private void StartSpawn()
@@ -77,42 +87,24 @@ public class PlayerControl : MonoBehaviour
 
     private void HitAndDie()
     {
-        // transform.position += velocity * Time.deltaTime;
-        // float fallPosition = Mathf.MoveTowards(transform.position.y, 0.0f, 3.0f);
-        hit = true;
-        float fallPosition = Mathf.Clamp(transform.position.y, 0.0f, 0.001f);
-        sound_hit.Play();
-        sound_die.Play();
-        velocity.y +=  my_gravity * Time.deltaTime;
-        transform.position += velocity * Time.deltaTime;
-        transform.localPosition = new Vector3(transform.position.x, velocity.y, transform.position.z);
-        // Time.timeScale = 1;
+        if (hit == false)
+        {
+            hit = true;
+            sound_hit.Play();
+            sound_die.Play();
+        }
+        Time.timeScale = 1;
     }
     private void GameOver()
     {
         gameOver = true;
         Debug.Log("Game Over!");
-
-        // velocity = Vector3.down * upforce;
-        // velocity.y -=  my_gravity * Time.deltaTime;
-        // transform.position -= velocity * Time.deltaTime;
-        // // transform.position += velocity * Time.deltaTime;
-        // // float fallPosition = Mathf.MoveTowards(transform.position.y, 0.0f, 1.0f);
-        // sound_hit.Play();
-        // sound_die.Play();
-        // // transform.eulerAngles = new Vector3(transform.position.x, fallPosition, transform.position.z);
-        // transform.position = Vector3.Lerp(transform.position, Vector3.down, 1);
-        // // upforce = 0;
-        // gameOver = true;
-        // Time.timeScale = 1;
-        // Debug.Log("Game Over!");
     }
 
     private void PlayersAction()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            fallSpeed = 0;
             sound_flap.Play();
             velocity = Vector3.up * upforce;
         }
