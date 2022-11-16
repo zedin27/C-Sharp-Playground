4994c895ee520dd0e1c17069c00f94bcb2684cf8
zedin27 <zeidtisnes@gmail.com>
2022-11-16 22:35:36 +0300

    update: added end screen after dying. There is a bug in my upper and lower bound yet to fix

--- everything/3D Project/Crossy Road/Assets/Scripts/GUIManager.cs
+++ everything/3D Project/Crossy Road/Assets/Scripts/GUIManager.cs
@@ -1,21 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class GUIManager : MonoBehaviour
 {
     public bool gameOver = false;
+    public RectTransform Panel;
+    [SerializeField] private PlayerControl2 playerControlScript;
+    [SerializeField] private GameObject gameOverPanel;
+    [SerializeField] private Text restartText;
 
-    // public bool _gameOver => gameOver;
-    // Start is called before the first frame update
     void Start()
     {
-        
+        gameOverPanel.SetActive(false);
+        restartText.gameObject.SetActive(false);
+        Panel.localScale = new Vector3(0, 0);
+        playerControlScript = GameObject.Find("ParentPlayer").GetComponent<PlayerControl2>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        DeathScreen();
+    }
+
+    public void DeathScreen()
+    {
+        // if (playerControlScript.hit || playerControlScript.gameOver || playerControlScript.carry)
+        // {
+        //     print("playerControlScript");
+        //     Panel.localScale = new Vector3(0.5f, 1, 0.5f);
+        //     gameOver = true;
+        //     playerControlScript.gameOver = gameOver;
+        //     StartCoroutine(GameOverSequence());
+        // }
+        if ((Input.GetKeyDown(KeyCode.G) && !gameOver))
+        {
+            print("Debug");
+            Panel.localScale = new Vector3(0.5f, 1, 0.5f);
+            gameOver = true;
+            playerControlScript.gameOver = gameOver;
+            StartCoroutine(GameOverSequence());
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            print("Quit");
+            Application.Quit();
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+            Panel.localScale = new Vector3(0.5f, 1, 0.5f);
+    }
+    private IEnumerator GameOverSequence()
+    {
+        gameOverPanel.SetActive(true);
+        yield return new WaitForSeconds(1.0f);
+        restartText.gameObject.SetActive(true);
     }
 }
--- everything/3D Project/Crossy Road/Assets/Scripts/PlayerControl2.cs
+++ everything/3D Project/Crossy Road/Assets/Scripts/PlayerControl2.cs
@@ -16,12 +16,17 @@ public class PlayerControl2 : MonoBehaviour
 
     public bool IsIdle => _isIdle;
     public bool HasFireFirstInput => _hasFireFirstInput;
-    public bool gameOver = false;
     public bool hit = false;
     public bool isDisabled = false;
+    private float upperBound = 9f; //Maximum range for player to move
+
+    //Gameover tutorial
+    public bool gameOver = false;
+    public bool outOfBounds;
+
     private void Update()
     {
-        if (!gameOver && !hit)
+        if (!gameOver)
         {
             if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
             {
@@ -31,63 +36,68 @@ public class PlayerControl2 : MonoBehaviour
                     currentLerpTime = 0;
                     _hasFireFirstInput = true;
                     _isIdle = false;
+                    outOfBounds = false;
 
-                    Vector3 pos = transform.position;
-                    gameObject.transform.position = Vector3.Lerp(startPos, endPos, changeRatio);
-                    pos = new Vector3(pos.x, pos.y, pos.z + .3f * Time.deltaTime);
+                    ActiveVector3();
                 }
             }
             else
                 _isIdle = true;
             if (Input.GetButtonDown("up") && gameObject.transform.position == endPos)
-            {
-                endPos = transform.position;
                 endPos = new Vector3(transform.position.x + 1.5f, transform.position.y, transform.position.z);
-            }
             if (Input.GetButtonDown("down") && gameObject.transform.position == endPos)
-            {
-                endPos = transform.position;
                 endPos = new Vector3(transform.position.x - 1.5f, transform.position.y, transform.position.z);
-            }
-            if (Input.GetButtonDown("left") && gameObject.transform.position == endPos)
-            {
-                endPos = transform.position;
+            if (Input.GetButtonDown("left") && gameObject.transform.position == endPos && !outOfBounds)
                 endPos = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1.5f);
-            }
-            if (Input.GetButtonDown("right") && gameObject.transform.position == endPos)
-            {
-                endPos = transform.position;
+            if (Input.GetButtonDown("right") && gameObject.transform.position == endPos && !outOfBounds)
                 endPos = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1.5f);
+            if (!outOfBounds)
+            {
+                if (transform.position.z >= upperBound)
+                {
+                    outOfBounds = true;
+                    transform.position = new Vector3(transform.position.x, transform.position.y, upperBound);
+                }
+                if (transform.position.z <= -upperBound)
+                {
+                    outOfBounds = true;
+                    transform.position = new Vector3(transform.position.x, transform.position.y, -upperBound);
+                }
             }
         }
         else
             hit = !hit;
     }
 
-    // void OnCollisionStay(Collision collision)
-    // {
-    //     foreach (ContactPoint contact in collision.contacts)
-    //         Debug.DrawRay(contact.point, contact.normal * 10, Color.white);
-    // }
+    private bool ActiveVector3()
+    {
+        if (!outOfBounds)
+        {
+            Vector3 pos = transform.position;
+            gameObject.transform.position = Vector3.Lerp(startPos, endPos, changeRatio);
+            pos = new Vector3(pos.x, pos.y, pos.z + .3f * Time.deltaTime);
+            return true;
+        }
+        return false;
+    } 
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Obstacle"))
-        {
             HitAndDie();
-            GameOver();
-        }
     }
-        private void HitAndDie()
+    private void HitAndDie()
     {
         if (hit == false)
         {
             hit = true;
-            print("Game Over!");
+            GameOver();
         }
         Time.timeScale = 1;
     }
-    private void GameOver()
+    public void GameOver()
     {
         gameOver = true;
+        print("Game Over!");
     }
 }
